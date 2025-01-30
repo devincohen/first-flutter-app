33eea578d75f0e05d259b4faeab4117b3844b9be
devincohen <devin94cohen@gmail.com>
2025-01-29 22:58:31 -0600

    Playing with UI dev

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,20 +9,51 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: false,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              onPressed: Scaffold.of(context).openDrawer, 
+              icon: Icon(Icons.abc_sharp));
+          }),
+        // leading: Icon(
+        //     Icons.abc_sharp
+        // )
+        title: Text("BetterLove"),
+        
+      ),
+      drawer: NavigationDrawer(
+        backgroundColor: Colors.green,
+        children: []
+      ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            var url = Uri.parse('https://pokeapi.co/api/v2/pokemon/ditto');
-            var response = await http.get(url);
-            print('Response status: ${response.statusCode}');
-            var data = jsonDecode(response.body);
-            print(data['abilities'][0]['ability']['name']);
-            String ability = data['abilities'][0]['ability']['name'];
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondScreen(ability: ability)));
-          },
-          child: Text("Go to next screen")
-          ),
-      )
+        child: Column(
+          children: [
+            Text("Test 1"),
+            Text("Test 2"),
+            Expanded(child: Center(
+              child: Text("Test 3"),
+              ),
+              ),
+              
+          ]
+        )
+        // child: ElevatedButton(
+        //   onPressed: () async {
+        //     var url = Uri.parse('https://pokeapi.co/api/v2/pokemon/ditto');
+        //     var response = await http.get(url);
+        //     print('Response status: ${response.statusCode}');
+        //     var data = jsonDecode(response.body);
+        //     print(data['abilities'][0]['ability']['name']);
+        //     String ability = data['abilities'][0]['ability']['name'];
+        //     Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondScreen(ability: ability)));
+        //   },
+        //   child: Text("Go to next screen")
+        //   ),
+      ),
       
     );
   }
